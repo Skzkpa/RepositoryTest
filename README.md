cde090737f692f71d2a78ec57f00a58b736c2479
Piotr Dyba <piotr@dyba.com.pl>
2019-10-24 19:40:37 +0200

    finishes readme moves script parts to a folder adds simple test

--- README.md
+++ README.md
@@ -30,9 +30,30 @@ npm run test
 npm run lint
 ```
 
+## Project setup - Python
+### Install Python dependencies
+```
+pip install -r ./src/data/requirements
+```
+### Run your tests
+There is a simple test writen in ./src/data/commons/tests.py
+```
+python tests.py
+```
+
+
 ## Building a task list.
 
 1. Open excel file in ./src/data/base_excel_file.xlsx 
 2. Add relevant information, it is fine to leave empty lines but all fields are expected.
 3. Copy the content of excel file except the header row
 4. Open ./src/data/excel_to_yaml.py 
+5. Past data to marked place from excel.
+6. Fill the velocity.
+7. Run the script:
+```
+python excel_to_yaml.py 
+```
+8. Replace the content of ./src/data/tasks.yaml with the outcome of above script. 
+9. Now You can adjust the Yaml directly.
+10. You can also run the development server with `npm run serve` to check if the outcome of You work is as expected.
--- src/data/excel_to_yaml.py
+++ src/data/excel_to_yaml.py
@@ -1,97 +1,18 @@
-import yaml
-from copy import deepcopy
-from datetime import date, timedelta
-from collections import defaultdict
-
 ### User input
 
 velocity = 30
 input_data = """
 
-
-<< Pass the data here >>
+<< Pass the data from excel here, remember to not copy the header >>
 
 
 """
 
 
 
-### Script is below
+### Script is below -- do NOT touch :)
+from commons import process
 
-styles = {
-    "apes": {
-        "base": {
-            "fill": "#0287D0",
-            "stroke": "#0077C0"
-        }
-    },
-    "monkeys": {
-        "base": {
-            "fill": "#8E44AD",
-            "stroke": "#7E349D"
-        }
-    },
-    "apes&monkeys": {
-        "base": {
-            "fill": "#1EBC61",
-            "stroke": "#0EAC51"
-        }
-    },
-    "blocked": dict(),
-}
-percent = {
-    "New": 0,
-    "Closed": 100,
-    "Active": 20,
-    "Blocked": 0,
-}
+print(process(input_data, velocity))
 
 
-def add_days_skipping_weekends(adate, days):
-    for i in range(days):
-        adate += timedelta(days=1 if adate.weekday else 3)
-    return adate
-
-
-def process(data, global_days_lengh=0, start=None):
-    full_data = []
-    data = data.split('\n')
-    all_data = defaultdict(list)
-    current_date = start if start else date(2019, 10, 16)
-    for line in data:
-        if not line:
-            continue
-        ticket, atitle, team, release, po, status, story_points = line.split('\t')
-        if global_days_lengh == 0:
-            days_lengh = round(int(story_points if story_points else 1) / (velocity / 10))
-            days_to_add = days_lengh if days_lengh > 0 else 1
-        else:
-            days_lengh = global_days_lengh
-            days_to_add = days_lengh
-        cdate = current_date
-        current_date = add_days_skipping_weekends(current_date, days_to_add)
-        base = {
-            "label": atitle,
-            "user": 2,
-            "team": team,
-            "ticket": ticket,
-            "start": str(cdate),
-            "duration": days_lengh if days_lengh > 0 else 1,
-            "story_points": story_points,
-            "status": status,
-            "percent": percent.get(status, 0),
-            "type": "project",
-            "style": deepcopy(styles.get(status, styles.get(team.lower() if team else "apes&monkeys")))
-        }
-        all_data[team].append(base)
-
-        i = 0
-        for k, val in all_data.items():
-            for v in val:
-                v["id"] = i
-                i += 1
-            full_data += val
-        return yaml.dump(full_data, default_flow_style=False)
-
-
-print(process(input_data))
